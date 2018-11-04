446894dedd29a26d0a2f0916fe7702ed3b211a9f
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2018-11-04 09:30:54 -0500

    fixed some compile errors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -5,35 +5,28 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     super();
   }
   public NoNullArrayList(int start){
-    size=start;
+    super(start);
   }
 
   public T set(int index, T value){
     if (value == null){
       throw new IllegalArgumentException("Value cannot be null");
     }
-    super.set();
-    return super.set(i,value);
+    return super.set(index,value);
   }
 
   public boolean add(T value){
    if (value == null) {
      throw new IllegalArgumentException("Value cannot be null") ;
    }
-   else {
-     super.add(value) ;
-     return true ;
-   }
    return super.add(value);
-   }
+  }
 
- public void add(int index, T value) {
+  public void add(int index, T value) {
    if (value == null) {
      throw new IllegalArgumentException("Value cannot be null") ;
    }
-   else {
-     super.add(index, value) ;
-   }
-}
+   super.add(index, value);
+  }
 
 }
--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,7 +5,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public OrderedArrayList(int start){
-    size=start;
+    super(start);
   }
 
   public T set(int index, T value){
@@ -22,19 +22,23 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     if (value == null) {
       throw new IllegalArgumentException("Value cannot be null") ;
     }
-    for(int i = 0; i < ; i++){
-      if(value.compareTo(this.get(i)) > 0){
-         super.add(value);
+    for(int i = 0; i < size(); i++){
+      if(value.compareTo(this.get(i)) <= 0){
+         super.add(i, value);
          return true ;
       }
     }
     super.add(0,value);
+    return true;
    }
+
 
   public void add(int index, T value) {
     if (value == null) {
       throw new IllegalArgumentException("Value cannot be null") ;
     }
-    super.add(index, value);
+    this.add(value);
   }
+
+
 }
